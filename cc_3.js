3297908683614cf33ae77ef3238fc1824588c1e4
Vincent Crooks <crooksv@usf.edu>
2025-09-14 22:22:25 -0400

    push, shift & updates

--- cc_3.js
+++ cc_3.js
@@ -2,7 +2,12 @@
 //Vincent Crooks
 
 let customers = [ 
-    { name: "Vinny Crooks", email: "crooksv@usf.edu" purchases: ["Tires", "oil change"] },
+    { name: "Vinny Crooks", email: "crooksv@usf.edu",purchases: ["Tires", "oil change"] },
     { name: "Jane Doe", email: "janethedoe@yahoo.com", purchases: ["Brakes", "Wipers"]},
-    {name: "Chloe Day", email: "Chloeday@gmail.com", purchases: ["Shocks", "Struts"]}}
-]
+    {name: "Chloe Day", email: "Chloeday@gmail.com", purchases: ["Shocks", "Struts"]}
+];
+
+customers.push({name:"Anthony Locke", email: "antlocke@usf.edu", purchases: ["Cola", "Oil"]});
+customers.shift();
+customers[0].email = "vinnyc@gmail.com";
+customers[1].purchases.push("Oil");
