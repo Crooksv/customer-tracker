360c6dc61ed054eaf52a13b75f616438f14f7c5e
Vincent Crooks <crooksv@usf.edu>
2025-09-14 22:11:56 -0400

    customer array

---
+++ cc_3.js
@@ -0,0 +1,8 @@
+//Coding Challenge 03a
+//Vincent Crooks
+
+let customers = [ 
+    { name: "Vinny Crooks", email: "crooksv@usf.edu" purchases: ["Tires", "oil change"] },
+    { name: "Jane Doe", email: "janethedoe@yahoo.com", purchases: ["Brakes", "Wipers"]},
+    {name: "Chloe Day", email: "Chloeday@gmail.com", purchases: ["Shocks", "Struts"]}}
+]
